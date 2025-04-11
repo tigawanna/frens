9a013488589874c38d7d06c16c521450e8674f55
tigawanna <denniskinuthiawaweru@gmail.com>
2025-04-12 00:35:01 +0300

    feat: add GraphQL endpoint to allowed origins and log allowed origins; enhance CORS configuration

--- api/src/middleware/cors-stuff.ts
+++ api/src/middleware/cors-stuff.ts
@@ -13,5 +13,8 @@ export const allowedOrigins = [
   "http://localhost:5000/api",
   "http://192.168.0.149:3000/",
   process.env.FRONTEND_URL ?? "",
+  `${process.env.FRONTEND_URL}/graphql`,
   process.env.API_URL ?? "",
 ];
+
+console.log("Allowed Origins", allowedOrigins);
