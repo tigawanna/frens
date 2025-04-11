2eab3828d7436b815059ec65a479b20dfd0a35d5
tigawanna <denniskinuthiawaweru@gmail.com>
2025-04-11 19:28:23 +0300

    feat: enhance profile route layout; improve structure and styling for frenId display

--- client/src/routes/profile/$frenId/index.tsx
+++ client/src/routes/profile/$frenId/index.tsx
@@ -1,19 +1,21 @@
 import { Suspense } from "react";
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute } from "@tanstack/react-router";
 // import { OtherFrenProfileContainer } from "./-components/OtherFrenProfileContainer";
 import { CardsListSuspenseFallback } from "@/components/wrappers/GenericDataCardsListSuspenseFallback";
 
-export const Route = createFileRoute('/profile/$frenId/')({
+export const Route = createFileRoute("/profile/$frenId/")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
   const { frenId } = Route.useParams();
-  
+
   return (
     <div className="min-h-screen w-full flex flex-col items-center gap-3">
       <Suspense fallback={<CardsListSuspenseFallback />}>
-      <h1 className="text-2xl font-bold">{frenId}</h1>
+        <div className="w-full max-w-6xl flex min-h-[60vh] flex-col items-center justify-center p-4">
+          <h1 className="text-2xl font-bold">{frenId}</h1>
+        </div>
         {/* <OtherFrenProfileContainer frenId={frenId} /> */}
       </Suspense>
     </div>
