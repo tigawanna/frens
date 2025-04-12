0160ee7fb8a2eb15fce380af339d6285a7a7aff7
tigawanna <denniskinuthiawaweru@gmail.com>
2025-04-12 14:07:00 +0300

    feat: enhance authentication logging; update Docker environment configuration and add production build script

--- api/src/middleware/cors-stuff.ts
+++ api/src/middleware/cors-stuff.ts
@@ -5,15 +5,13 @@ export function corsHeaders(req: Request, res: Response, next: NextFunction) {
   if (allowedOrigins.includes(req.headers.origin)) {
     // res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
     res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
     next();
   }
 }
 export const allowedOrigins = [
-  "http://localhost:5000/api",
-  "http://192.168.0.149:3000/",
   process.env.FRONTEND_URL ?? "",
-  `${process.env.FRONTEND_URL}/graphql`,
+`${process.env.FRONTEND_URL}/graphql`,
   process.env.API_URL ?? "",
 ];
 
