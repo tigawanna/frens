5122bf914515870be79d1cc17c8ba01641764f2d
tigawanna <denniskinuthiawaweru@gmail.com>
2025-04-14 02:01:04 +0300

    refactor: remove unused imports in index.tsx

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,9 +1,7 @@
-import { MainNavbar } from "@/components/navigation/navbar/MainNavbar";
-import { createFileRoute, Link } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 import { MainDrawer } from "../components/navigation/drawer/MainDrawer";
 import { MainDrawerLinks } from "../components/navigation/drawer/MainDrawerLinks";
 import { MainDrawerFooter } from "@/components/navigation/drawer/MainDrawerFooter";
-import { BulkusresCreate } from "./admin/-components/BulkusresCreate";
 import MainFeedWrapper from "./-components/feed/MainFeedContainer";
 
 export const Route = createFileRoute("/")({
